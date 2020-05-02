a360072d95423e91ca15a3107bbd38502f151525
Paulo Ricardo Lacerda <paulodassie.lacerda@gmail.com>
2020-05-01 22:15:21 -0400

    Initial swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../config/swagger.json';
+
 import toolsRouter from './tools.routes';
 
 const routes = Router();
 
 routes.use('/tools', toolsRouter);
+
+routes.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default routes;
